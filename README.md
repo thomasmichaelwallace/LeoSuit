1c9ff07744068b9932950986c1ac06b16b4c8d64
thomasmichaelwallace <thomasmichaelwallace@gmail.com>
2024-06-01 16:19:09 +0100

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Leo's Suit
+
+Set the colour of Leo's suit (/van) to the colour on your badge!
---
+++ apps/leosuit/app.py
@@ -0,0 +1,57 @@
+import app
+import requests
+
+from app_components import clear_background
+from system.eventbus import eventbus
+from events.input import Buttons, BUTTON_TYPES
+from tildagonos import tildagonos
+from system.patterndisplay.events import PatternDisable
+
+URL = "https://roygbivw.hecknswell.com/colour/"
+
+class LeoSuit(app.App):
+    def __init__(self):
+        eventbus.emit(PatternDisable())
+        self.button_states = Buttons(self)
+        self.colour = requests.get(URL).json()
+        print("init", self.colour)
+
+    def update(self, delta):
+        tildagonos.leds[2] = (255, 0, 0)
+        tildagonos.leds[3] = (255, 0, 0)
+        tildagonos.leds[8] = (0, 255, 0)
+        tildagonos.leds[9] = (0, 255, 0)
+        tildagonos.leds[12] = (0, 0, 255)
+        tildagonos.leds[1] = (0, 0, 255)
+        tildagonos.leds[6] = (255, 255, 0)
+        tildagonos.leds[7] = (255, 255, 0)
+        tildagonos.leds[10] = (0, 255, 255)
+        tildagonos.leds[11] = (0, 255, 255)
+        tildagonos.leds[4] = (255, 0, 255)
+        tildagonos.leds[5] = (255, 0, 255)
+        data = self.colour
+        if self.button_states.get(BUTTON_TYPES["RIGHT"]):
+            data = { "r": 255, "g": 0, "b": 0}
+        elif self.button_states.get(BUTTON_TYPES["LEFT"]):
+            data = { "r": 0, "g": 255, "b": 0}
+        elif self.button_states.get(BUTTON_TYPES["UP"]):
+            data = { "r": 0, "g": 0, "b": 255}
+        elif self.button_states.get(BUTTON_TYPES["DOWN"]):
+            data = { "r": 255, "g": 255, "b": 0}
+        elif self.button_states.get(BUTTON_TYPES["CANCEL"]):
+            data = { "r": 0, "g": 255, "b": 255}
+        elif self.button_states.get(BUTTON_TYPES["CONFIRM"]):
+            data = { "r": 255, "g": 0, "b": 255}
+
+        if data["r"] != self.colour["r"] or data["g"] != self.colour["g"] or data["b"] != self.colour["b"]:
+            print("putting", self.colour, "to", data)
+            self.colour = data
+            requests.post(URL, json=self.colour)
+            print("put!")
+
+    def draw(self, ctx):
+        clear_background(ctx)
+        ctx.rgb(self.colour['r'], self.colour['g'], self.colour['b'])
+        ctx.rectangle(-150, -150, 300, 300).fill()
+
+__app_export__ = LeoSuit
